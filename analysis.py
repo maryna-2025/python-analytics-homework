69125bd42d12040fb65b3bd987f3ce5bc74dc041
test <maryna.klymko.uk@gmail.com>
2026-02-20 14:01:04 +0200

    Deleting min value from conclusions

--- analysis.py
+++ analysis.py
@@ -5,5 +5,5 @@ print("Продажі по містах:")
 print(df)
 print("Середнє значення:", round(df["sales"].mean(), 2))
 print("Найвищі продажі:" , round(df['sales'].max(), 2))
-print('Найнижчі продажі:', round(df['sales'].min(), 2) )
+
 
